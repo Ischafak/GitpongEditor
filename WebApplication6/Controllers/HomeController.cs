8d17e73363568dd0f0b38c2c6b1039d7cd7a0a04
ischafak <safakismet@gmail.com>
2018-07-05 16:27:21 +0300

    ilk commit

--- WebApplication6/Controllers/HomeController.cs
+++ WebApplication6/Controllers/HomeController.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+using System.IO;
 
 namespace WebApplication6.Controllers
 {
@@ -15,7 +19,20 @@ namespace WebApplication6.Controllers
         }
         public ActionResult Xslt()
         {
-
+            return View();
         }
+
+        public ActionResult Xml()
+        {
+
+
+            ViewBag.mesaj = System.IO.File.ReadAllText("C:\\Users\\ismet\\source\\repos\\XsltCreator-1\\WebApplication6\\lib\\qqqq.xml");
+           
+
+
+
+            return View();
+        }
+      
     }
 }
